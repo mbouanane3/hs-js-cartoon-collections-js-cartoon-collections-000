9ca14cbe7425fd503b4db89884071c3581e428e6
mohammed bouanane <mbouanane3@gmail.com>
2020-03-03 17:34:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves) { 
+var string = '';
+  dwarves.forEach(function(name, index ) {
+    string += (index+1+'.'+' '+name+' ');
+    });
+    return string;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  return planeteerCalls.map(function(element){
+    return element.toUpperCase()+ '!';
+  });
 }
 
 function longPlaneteerCalls(words) {
-}
+	for(var i = 0; i < words.length; i++) {
+		if(words[i].length > 4) {
+			return true;
+		}else{
+	}
+	return false;}}
 
-function findTheCheese (foods) {
-}
+ function findTheCheese (foods) {
+ 	var cheese = ["camembert", "cheddar", "gouda"];
+	for(var i = 0; i < foods.length; i++) {
+ 		for(var j = i; j < cheese.length; j++){
+			if(foods[i] === cheese[j]) {
+ 				return cheese[j];
+ 			}
+ 		} 	}
+ 	return "no cheese!";
+ }
